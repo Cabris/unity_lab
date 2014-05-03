6b2ef4ede08bfdf3f8e93c12bac5f5cee9b0766c
Cabris <christtabris@gmail.com>
2014-05-04 05:32:56 +0800

    prefab path

---
+++ unity_game/Assets/Scripts/SceneController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+public class SceneController : MonoBehaviour {
+
+	public List<GameObject> sceneObjs=new List<GameObject>();
+	
+	// Use this for initialization
+	void Start () {
+		NetworkTransformSender[] s=GetComponentsInChildren<NetworkTransformSender>();
+		foreach(NetworkTransformSender ss in s){
+			GameObject g=ss.gameObject;
+			sceneObjs.Add(g);
+			GameObject p=PrefabUtility.GetPrefabParent(g) as GameObject;
+			string path=AssetDatabase.GetAssetPath(p);
+			Debug.Log(g.name+", "+path+".");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
